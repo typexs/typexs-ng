8d56c312dcfe077f0830cbbecd01bf88f01780be
Robert Kania <cezaryrk@yahoo.de>
2018-02-08 11:32:27 +0100

    update

--- test/development/coolapp/src/modules/app/app.module.ts
+++ test/development/coolapp/src/modules/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {AppComponent} from './app.component';
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {APP_ROUTES} from "./app.routes";
 
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test/development/coolapp/src/modules/app/app.routes.ts
+++ test/development/coolapp/src/modules/app/app.routes.ts
@@ -2,7 +2,7 @@
 
 import {Routes} from "@angular/router";
 
-export const appRoutes: Routes = [
+export const APP_ROUTES: Routes = [
   /*
   {path: 'crisis-center', component: CrisisListComponent},
   {path: 'hero/:id', component: HeroDetailComponent},
