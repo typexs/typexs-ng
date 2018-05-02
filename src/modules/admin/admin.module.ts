c31ffd92ec827b930906ee331839e472f4dc3c1e
Robert Kania <cezaryrk@yahoo.de>
2018-05-02 12:03:42 +0200

    update pcakages

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,4 +1,4 @@
-import {BrowserModule} from '@angular/platform-browser';
+
 import {NgModule} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {AdminComponent} from "./admin.component";
