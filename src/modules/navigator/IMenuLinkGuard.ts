36c9ca7966dbcb8c120578994908e205dc2bff51
Robert Kania <cezaryrk@yahoo.de>
2018-11-27 14:21:25 +0100

    fix: Link wasn't shown

--- src/modules/navigator/IMenuLinkGuard.ts
+++ src/modules/navigator/IMenuLinkGuard.ts
@@ -2,7 +2,7 @@ import {NavEntry} from './NavEntry';
 
 export interface IMenuLinkGuard {
 
-  isDisabled?(entry: NavEntry): Promise<boolean> | boolean;
+  isDisabled?(entry: NavEntry): boolean;
 
-  isHidden?(entry: NavEntry): Promise<boolean> | boolean;
+  isHidden?(entry: NavEntry): boolean;
 }
--- src/modules/navigator/menu-link.component.ts
+++ src/modules/navigator/menu-link.component.ts
@@ -2,48 +2,70 @@ import * as _ from 'lodash';
 import {Component, Injector, Input, OnInit} from '@angular/core';
 import {INavTreeEntry} from './INavTreeEntry';
 import {IMenuLinkGuard} from './IMenuLinkGuard';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'menu-link',
   templateUrl: './menu-link.component.html',
   styleUrls: ['./menu-link.component.scss']
 })
-export class MenuLinkComponent {
+export class MenuLinkComponent implements OnInit {
 
   @Input()
   entry: INavTreeEntry;
 
 
-  activators:IMenuLinkGuard[] = null;
+  activators: IMenuLinkGuard[] = null;
 
-  constructor(private injector: Injector){}
+
+
+  constructor(private injector: Injector) {
+  }
+
+  ngOnInit(): void {
+
+  }
 
   hasChildren() {
     return this.entry.children && this.entry.children.length > 0;
   }
 
-  private getActivator(){
-    if(this.activators) return this.activators;
+  private getActivator() {
+    if (this.activators) return this.activators;
     this.activators = [];
     let canActivate = _.get(this.entry, 'entry.route.canActivate', false);
     if (canActivate && _.isArray(canActivate)) {
-      for(let canAct of canActivate){
+      for (let canAct of canActivate) {
         let guard = this.injector.get(canAct);
-        if(guard.isDisabled || guard.isHidden){
-          this.activators.push(guard)
+        if (guard.isDisabled || guard.isHidden) {
+          this.activators.push(guard);
         }
       }
     }
     return this.activators;
   }
 
-  async isDisabled() {
-    let disable:boolean = false;
+  isDisabled() {
+    let disable: boolean = false;
     let activators = this.getActivator();
     if (!_.isEmpty(activators)) {
-      for(let canAct of activators){
-        if(canAct.isDisabled){
-          disable = disable || await (<IMenuLinkGuard>canAct).isDisabled(this.entry.entry);
+      for (let canAct of activators) {
+        if (canAct.isDisabled) {
+          disable = disable ||  (<IMenuLinkGuard>canAct).isDisabled(this.entry.entry);
+        }
+      }
+    }
+    return disable;
+  }
+
+  isHidden() {
+    let disable: boolean = false;
+    let activators = this.getActivator();
+    if (!_.isEmpty(activators)) {
+      for (let canAct of activators) {
+        if (canAct.isHidden) {
+          disable = disable ||  (<IMenuLinkGuard>canAct).isHidden(this.entry.entry);
         }
       }
     }
@@ -51,16 +73,4 @@ export class MenuLinkComponent {
   }
 
 
-  async isHidden() {
-    let hidden:boolean = false;
-    let activators = this.getActivator();
-    if (!_.isEmpty(activators)) {
-      for(let canAct of activators){
-        if(canAct.isHidden){
-          hidden = hidden || await (<IMenuLinkGuard>canAct).isHidden(this.entry.entry);
-        }
-      }
-    }
-    return hidden;
-  }
 }
