aa46f6d1655bc5547760df3027d27efa74f354fe
Robert Kania <cezaryrk@yahoo.de>
2018-12-18 15:08:19 +0100

    fix: checkbox options

--- src/libs/forms/decorators/FormCheckbox.ts
+++ src/libs/forms/decorators/FormCheckbox.ts
@@ -1,10 +1,16 @@
 import {OptionsHelper} from '@typexs/schema/libs/registry/OptionsHelper';
 import {ClassRef} from '@typexs/schema/libs/registry/ClassRef';
 import {ISelectOptions} from '../elements/ISelectOptions';
+import {ICheckboxOptions} from '../elements/ICheckboxOptions';
 
-export function FormCheckbox() {
+export function FormCheckbox(opts: ICheckboxOptions = {}) {
   return function (object: any, property: string) {
     // use enum attribute, but later it will be deprecated
-    OptionsHelper.forPropertyOn(ClassRef.get(object), property, {form: 'checkbox'});
+   let _opts:any = {form: 'checkbox'};
+   if(opts && opts.enum){
+     _opts.enum = opts.enum;
+   }
+
+    OptionsHelper.forPropertyOn(ClassRef.get(object), property, _opts);
   };
 }
---
+++ src/libs/forms/elements/ICheckboxOptions.ts
@@ -0,0 +1,4 @@
+export interface ICheckboxOptions {
+  enum?: string | Function;
+}
+
