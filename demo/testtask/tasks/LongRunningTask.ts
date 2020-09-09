90861b9f6ed63c7a60d60d89901a2c1329c1b526
Robert Kania <cezaryrk@yahoo.de>
2020-09-09 20:16:15 +0200

    demo: add task parameters

--- demo/testtask/tasks/LongRunningTask.ts
+++ demo/testtask/tasks/LongRunningTask.ts
@@ -1,4 +1,5 @@
 import {ITask, ITaskRuntimeContainer, TaskRuntime} from '@typexs/base';
+import {Incoming} from '@typexs/base/index';
 
 export class LongRunningTask implements ITask {
 
@@ -6,6 +7,14 @@ export class LongRunningTask implements ITask {
   r: ITaskRuntimeContainer;
 
   name = 'long_running_task';
+
+
+  @Incoming({optional: true})
+  iterations: number = 100;
+
+  @Incoming({optional: true})
+  timeout: number = 1000;
+
 
   wait(time: number) {
     return new Promise(resolve => {
@@ -17,13 +26,13 @@ export class LongRunningTask implements ITask {
 
 
   async exec() {
-    this.r.total(1000);
+    this.r.total(this.iterations);
     const l = this.r.logger();
 
     const res: any = {inc: 0};
     let i = 0;
-    while (i < 1000) {
-      await this.wait(1000);
+    while (i < this.iterations) {
+      await this.wait(this.timeout);
       i++;
       res.inc = i;
       l.info('doing ' + i + ' iteration');
