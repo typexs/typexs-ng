8290d7c10cfb4dac4a5f4da712f01c8d1c50b863
Robert Kania <cezaryrk@yahoo.de>
2020-09-29 18:31:03 +0200

    refactor: switch to jasmine test

--- karma.conf.js
+++ karma.conf.js
@@ -4,47 +4,72 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: 'src',
-    exclude: ['test/**'],
-    frameworks: ['mocha', 'chai', 'sinon-chai', '@angular-devkit/build-angular'],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-mocha'),
-      require('karma-chai'),
-//      require('karma-es6-shim'),
-      require('karma-sinon'),
-      require('karma-sinon-chai'),
+      require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-mocha-reporter'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage/karma'),
-      reports: ['html', "json",'lcovonly'],
+      dir: require('path').join(__dirname, './coverage/karma'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-
-
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    proxies: {
-      "/api": "http://localhost:4500/api"
-    },
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: false,
-    // preprocessors: {
-    //  './src/app/test.ts': ['@angular-devkit/build-angular']
-    // },
-    reporters: ['mocha'],
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
-    },
+    restartOnFileChange: true
+//
+//     basePath: 'src',
+//     exclude: ['test/**'],
+//     frameworks: ['mocha', 'chai', 'sinon-chai', '@angular-devkit/build-angular'],
+//     plugins: [
+//       require('karma-chai'),
+// //      require('karma-es6-shim'),
+//       require('karma-sinon'),
+//       require('karma-sinon-chai'),
+//       require('karma-chrome-launcher'),
+//       require('karma-mocha-reporter'),
+//       require('karma-coverage-istanbul-reporter'),
+//       require('@angular-devkit/build-angular/plugins/karma')
+//     ],
+//     client: {
+//       clearContext: false // leave Jasmine Spec Runner output visible in browser
+//     },
+//
+//     coverageIstanbulReporter: {
+//       dir: require('path').join(__dirname, 'coverage/karma'),
+//       reports: ['html', "json",'lcovonly'],
+//       fixWebpackSourcePaths: true
+//     },
+//
+//
+//     port: 9876,
+//     proxies: {
+//       "/api": "http://localhost:4500/api"
+//     },
+//     colors: true,
+//     logLevel: config.LOG_INFO,
+//     autoWatch: true,
+//     browsers: ['ChromeHeadless'],
+//     singleRun: false,
+//     // preprocessors: {
+//     //  './src/app/test.ts': ['@angular-devkit/build-angular']
+//     // },
+//     reporters: ['mocha'],
+//     mime: {
+//       'text/x-typescript': ['ts', 'tsx']
+//     },
   });
 };
 
