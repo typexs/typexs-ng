36abdb0ed8b2bda3f1a0efc5041781d066a321a4
Robert Kania <cezaryrk@yahoo.de>
2021-01-08 18:54:52 +0100

    build: mocha

--- .mocharc.js
+++ .mocharc.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Here's a JavaScript-based config file.
-// If you need conditional logic, you might want to use this type of config.
-// Otherwise, JSON or YAML is recommended.
-
 module.exports = {
   diff: true,
   // extension: ['js'],
