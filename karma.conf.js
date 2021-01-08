346d08c03d03a797208fda619ac4036256a97254
Robert Kania <cezaryrk@yahoo.de>
2021-01-08 18:54:40 +0100

    build: karma

--- karma.conf.js
+++ karma.conf.js
@@ -29,47 +29,6 @@ module.exports = function (config) {
     browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
-//
-//     basePath: 'src',
-//     exclude: ['test/**'],
-//     frameworks: ['mocha', 'chai', 'sinon-chai', '@angular-devkit/build-angular'],
-//     plugins: [
-//       require('karma-chai'),
-// //      require('karma-es6-shim'),
-//       require('karma-sinon'),
-//       require('karma-sinon-chai'),
-//       require('karma-chrome-launcher'),
-//       require('karma-mocha-reporter'),
-//       require('karma-coverage-istanbul-reporter'),
-//       require('@angular-devkit/build-angular/plugins/karma')
-//     ],
-//     client: {
-//       clearContext: false // leave Jasmine Spec Runner output visible in browser
-//     },
-//
-//     coverageIstanbulReporter: {
-//       dir: require('path').join(__dirname, 'coverage/karma'),
-//       reports: ['html', "json",'lcovonly'],
-//       fixWebpackSourcePaths: true
-//     },
-//
-//
-//     port: 9876,
-//     proxies: {
-//       "/api": "http://localhost:4500/api"
-//     },
-//     colors: true,
-//     logLevel: config.LOG_INFO,
-//     autoWatch: true,
-//     browsers: ['ChromeHeadless'],
-//     singleRun: false,
-//     // preprocessors: {
-//     //  './src/app/test.ts': ['@angular-devkit/build-angular']
-//     // },
-//     reporters: ['mocha'],
-//     mime: {
-//       'text/x-typescript': ['ts', 'tsx']
-//     },
   });
 };
 
