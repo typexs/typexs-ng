be6328d7a40b56ac9f0908d588f2fbdabfad97a8
Robert Kania <cezaryrk@yahoo.de>
2021-02-07 15:09:09 +0100

    build: update packages

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,13 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverage: {
       dir: require('path').join(__dirname, './coverage/karma'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
